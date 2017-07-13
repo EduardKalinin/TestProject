815e9ebe155eb081a30d1cd742faba9715b4ebc5
Eduard <eduard.kalinin.1993@gmail.com>
2017-07-13 19:09:30 +0300

    Added the logic of the transition between the text fields.

--- TestProject/Controllers/UserInfoViewController.m
+++ TestProject/Controllers/UserInfoViewController.m
@@ -25,17 +25,77 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Title";
+    [self configureButtonWithPhoneTextField];
+    [self configureButtonWithZipTextField];
+}
+
+#pragma mark - Helpers
+
+- (void)configureButtonWithPhoneTextField {
+    UIToolbar *keyboardNextButtonView = [[UIToolbar alloc] init];
+    [keyboardNextButtonView sizeToFit];
+    UIBarButtonItem *nextButton = [[UIBarButtonItem alloc] initWithTitle:@"Next"
+                                                                   style:UIBarButtonItemStylePlain target:self
+                                                                  action:@selector(nextClicked:)];
+    
+    UIBarButtonItem *leftSpaceButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    [keyboardNextButtonView setItems:[NSArray arrayWithObjects:leftSpaceButton, nextButton, nil]];
+    self.phoneTextField.inputAccessoryView = keyboardNextButtonView;
+}
+
+- (void)configureButtonWithZipTextField {
+    UIToolbar *keyboardNextButtonView = [[UIToolbar alloc] init];
+    [keyboardNextButtonView sizeToFit];
+    UIBarButtonItem *nextButton = [[UIBarButtonItem alloc] initWithTitle:@"Next"
+                                                                   style:UIBarButtonItemStylePlain target:self
+                                                                  action:@selector(nextClickedWithZip:)];
+    
+    UIBarButtonItem *leftSpaceButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    [keyboardNextButtonView setItems:[NSArray arrayWithObjects:leftSpaceButton, nextButton, nil]];
+    self.zipTextField.inputAccessoryView = keyboardNextButtonView;
+}
+
+
+#pragma mark - Actions
+
+- (IBAction)nextClicked:(id)sender {
+    if (self.phoneTextField) {
+        [self.addressTextField becomeFirstResponder];
+    }
+}
+
+- (IBAction)nextClickedWithZip:(id)sender {
+    if (self.zipTextField) {
+        [self.emailTextField becomeFirstResponder];
+    }
 }
 
 #pragma mark - UITextFieldDelegate
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     if ([textField isEqual:self.phoneTextField] || [textField isEqual:self.zipTextField]) {
-        NSCharacterSet *numbersOnly = [NSCharacterSet characterSetWithCharactersInString:@"0123456789"];
+        NSCharacterSet *numbersOnly = [NSCharacterSet decimalDigitCharacterSet];
         NSCharacterSet *characterSetFromTextField = [NSCharacterSet characterSetWithCharactersInString:textField.text];
         
         BOOL stringIsValid = [numbersOnly isSupersetOfSet:characterSetFromTextField];
         return stringIsValid;
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if ([textField isEqual:self.nameTextField]) {
+        [self.sexTextField becomeFirstResponder];
+    } else if ([textField isEqual:self.sexTextField]) {
+        [self.phoneTextField becomeFirstResponder];
+    } else if ([textField isEqual:self.addressTextField]) {
+        [self.zipTextField becomeFirstResponder];
+    } else if ([textField isEqual:self.emailTextField]) {
+        [self.passwordTextField becomeFirstResponder];
+    } else if ([textField isEqual:self.passwordTextField]) {
+        [self.passwordTextField resignFirstResponder];
     }
     return YES;
 }
