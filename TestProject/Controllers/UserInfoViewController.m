62ee6b59ccc2685cf2cf9c5ac7d19aca50062993
Eduard <eduard.kalinin.1993@gmail.com>
2017-07-13 17:20:15 +0300

    Added outlets to text field. Added validation for data entry. There are restrictions on the phone and zip.

--- TestProject/Controllers/UserInfoViewController.m
+++ TestProject/Controllers/UserInfoViewController.m
@@ -8,7 +8,15 @@
 
 #import "UserInfoViewController.h"
 
-@interface UserInfoViewController ()
+@interface UserInfoViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *sexTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+@property (weak, nonatomic) IBOutlet UITextField *addressTextField;
+@property (weak, nonatomic) IBOutlet UITextField *zipTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
 @end
 
@@ -16,7 +24,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = [NSString stringWithFormat:@"Title"];
+    self.title = @"Title";
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if ([textField isEqual:self.phoneTextField] || [textField isEqual:self.zipTextField]) {
+        NSCharacterSet *numbersOnly = [NSCharacterSet characterSetWithCharactersInString:@"0123456789"];
+        NSCharacterSet *characterSetFromTextField = [NSCharacterSet characterSetWithCharactersInString:textField.text];
+        
+        BOOL stringIsValid = [numbersOnly isSupersetOfSet:characterSetFromTextField];
+        return stringIsValid;
+    }
+    return YES;
 }
 
 @end
