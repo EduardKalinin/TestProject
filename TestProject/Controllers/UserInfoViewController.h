3ad728e12a8b14e8df13979cc15a57fbf41c9d7c
Eduard <eduard.kalinin.1993@gmail.com>
2017-07-13 15:49:33 +0300

    Customized the screen to fill user data.

---
+++ TestProject/Controllers/UserInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserInfoViewController.h
+//  TestProject
+//
+//  Created by Developer on 7/13/17.
+//  Copyright © 2017 Developer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface UserInfoViewController : ViewController
+
+@end
---
+++ TestProject/Controllers/UserInfoViewController.m
@@ -0,0 +1,22 @@
+//
+//  UserInfoViewController.m
+//  TestProject
+//
+//  Created by Developer on 7/13/17.
+//  Copyright © 2017 Developer. All rights reserved.
+//
+
+#import "UserInfoViewController.h"
+
+@interface UserInfoViewController ()
+
+@end
+
+@implementation UserInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = [NSString stringWithFormat:@"Title"];
+}
+
+@end
