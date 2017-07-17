896b92546814fadd9500f000b63ceebbdeb49e52
Eduard <eduard.kalinin.1993@gmail.com>
2017-07-17 13:34:13 +0300

    Added new storyboard. Added push and modal shows screens. Added a navigation to the screen which is shown modally and added the logic of closing this screen.

---
+++ TestProject/Controllers/NewSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewSecondViewController.h
+//  TestProject
+//
+//  Created by Developer on 7/17/17.
+//  Copyright © 2017 Developer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface NewSecondViewController : ViewController
+
+@end
---
+++ TestProject/Controllers/NewSecondViewController.m
@@ -0,0 +1,25 @@
+//
+//  NewSecondViewController.m
+//  TestProject
+//
+//  Created by Developer on 7/17/17.
+//  Copyright © 2017 Developer. All rights reserved.
+//
+
+#import "NewSecondViewController.h"
+
+@interface NewSecondViewController ()
+
+@end
+
+@implementation NewSecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+- (IBAction)cancelBarButtonAction:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
